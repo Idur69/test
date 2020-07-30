d74caf63efec6170c76b9585e65619133093ce28
Idur69 <shaikidurbasha469@gmail.com>
2020-07-30 07:10:02 +0530

    automation

--- test_exp.py
+++ test_exp.py
@@ -3,5 +3,7 @@ print("Hello idur this test for git")
 test = input("Are you Tester")
 if test == "yes":
     print("Do test very well!")
+    print("Do automation also")
 else:
     print("Go to sleep")
+    
