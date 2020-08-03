2a2b37dbbc8432f18f9655a8249ad4afbce1b2f7
Idur69 <shaikidurbasha469@gmail.com>
2020-08-03 09:31:41 +0530

    burger

---
+++ hungry.py
@@ -0,0 +1,11 @@
+print("Hello idur this test for git")
+
+test = input("Are you hungry?")
+if test == "yes":
+    print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
+    print("eat fries")
+else:
+    print("Do you exercise")
+
