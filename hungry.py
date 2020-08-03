2748da33e8ccf4eb8a8aa199a7a9dd26fb96b4f4
Idur69 <shaikidurbasha469@gmail.com>
2020-08-03 10:00:45 +0530

    thirsty

---
+++ hungry.py
@@ -0,0 +1,15 @@
+print("Hello idur this test for git")
+
+test = input("Are you hungry")
+if test == "yes":
+    print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
+else:
+    th = input("are you thirsty?")
+    if th == "yes":
+        print("drink water")
+        print("drink juice")
+
+
+
